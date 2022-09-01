c3ebad077e9de6cb4040149243a9b8f2f606da82
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-09-01 23:26:25 +0700

    response prevent

--- before-actions/iot.php
+++ before-actions/iot.php
@@ -3,12 +3,14 @@
 if(!isset($_GET['serial_number']))
 {
     echo iot_response('Unauthorized');
+    die();
     return false;
 }
 
 if(!isset($_GET['code']))
 {
     echo iot_response('Unauthorized');
+    die();
     return false;
 }
 
@@ -21,6 +23,7 @@ $device = $db->single('devices',['serial_number' => $serial_number]);
 if(empty($device))
 {
     echo iot_response('Unauthorized');
+    die();
     return false;
 }
 
@@ -30,6 +33,7 @@ $subject = $db->single('subjects',['presence_code'=>$code]);
 if(empty($subject))
 {
     echo iot_response('Unauthorized');
+    die();
     return false;
 }
 
